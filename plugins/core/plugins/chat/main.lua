e7175433c38ef75307a7e43d4340129fbdcaef03
Mr0maks <mr.maks0443@gmail.com>
2020-09-03 02:26:36 +0500

    Add delay for chat

--- plugins/core/plugins/chat/main.lua
+++ plugins/core/plugins/chat/main.lua
@@ -4,12 +4,24 @@ if MOD_PATH == "cstrike" then
 	error("plugin not working with CS 1.6")
 end
 
+local globalvars = require("global_vars")
 local meta_global = require("metamod")
 
+--Original value from hlsdk
+local chat_interval = 1.0
 local say_text = engine.reg_user_msg("SayText", -1);
+
+local player_next_chat_message = {}
 
 cmd.register("client", "say", function (executor, args, split_args)
 	local name = get_entity_keyvalue(executor, 'name');
+
+	if player_next_chat_message[name] > globalvars.time then
+		return
+	end
+
+	player_next_chat_message[name] = globalvars.time + chat_interval
+
 	local string_msg = string.format("%c[%s] %s: %s\n", 2, os.date("%X"), name, string.gsub(split_args, '"', ''));
 
 	if MOD_PATH ~= "cstrike" then
@@ -23,4 +35,14 @@ cmd.register("client", "say", function (executor, args, split_args)
 	meta_global.mres = metamod.MRES_SUPERCEDE
 
 	print(string_msg)
+end)
+
+engine_callback.register('pfnClientPutInServer', function (E)
+	local name = get_entity_keyvalue(executor, 'name');
+	player_next_chat_message[name] = 0
+end)
+
+engine_callback.register('pfnClientDisconnect', function (E)
+	local name = get_entity_keyvalue(executor, 'name');
+	player_next_chat_message[name] = nil
 end)
