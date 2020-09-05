dac169fd550a05f404498ec01d3072b0fb2f3f08
Mr0maks <mr.maks0443@gmail.com>
2020-09-05 13:25:37 +0500

    rework engine_callback.lua

--- plugins/core/engine_callback.lua
+++ plugins/core/engine_callback.lua
@@ -6,112 +6,33 @@ function engine_callback.register(engine_callback_name, callback)
   table.insert(engine_callback.callbacks[engine_callback_name], callback)
 end
 
-engine_events['pfnSpawn'] = function(E)
-  if engine_callback.callbacks['pfnSpawn'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnSpawn'] do
-    pcall(engine_callback.callbacks['pfnSpawn'][i], E)
-  end
+local function do_action(name, ...)
+  if engine_callback.callbacks[name] == nil then return end
+
+    for k,v in ipairs(engine_callback.callbacks[name]) do
+      v(...)
+    end
 end
 
-engine_events['pfnThink'] = function(E)
-  if engine_callback.callbacks['pfnThink'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnThink'] do
-    pcall(engine_callback.callbacks['pfnThink'][i], E)
-  end
-end
+engine_events['pfnSpawn'] = function(...) do_action('pfnSpawn', ...) end
+engine_events['pfnThink'] = function(...) do_action('pfnThink', ...) end
+engine_events['pfnUse'] = function(...) do_action('pfnUse', ...) end
+engine_events['pfnTouch'] = function(...) do_action('pfnTouch', ...) end
+engine_events['pfnBlocked'] = function(...) do_action('pfnBlocked', ...) end
+engine_events['pfnKeyValue'] = function(...) do_action('pfnKeyValue', ...) end
 
-engine_events['pfnUse'] = function(E, E2)
-  if engine_callback.callbacks['pfnUse'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnUse'] do
-    pcall(engine_callback.callbacks['pfnUse'][i], E, E2)
-  end
-end
+engine_events['pfnClientConnect'] = function(...) do_action('pfnClientConnect', ...) end
+engine_events['pfnClientDisconnect'] = function(...) do_action('pfnClientDisconnect', ...) end
+engine_events['pfnClientKill'] = function(...) do_action('pfnClientKill', ...) end
+engine_events['pfnClientPutInServer'] = function(...) do_action('pfnClientPutInServer', ...) end
+engine_events['pfnClientUserInfoChanged'] = function (...) do_action('pfnClientUserInfoChanged', ...) end
+engine_events['pfnClientCommand'] = function(...) do_action('pfnClientCommand', ...) end
 
-engine_events['pfnTouch'] = function(E, E2)
-  if engine_callback.callbacks['pfnTouch'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnTouch'] do
-    pcall(engine_callback.callbacks['pfnTouch'][i], E, E2)
-  end
-end
-
-engine_events['pfnBlocked'] = function(E,E2)
-  if engine_callback.callbacks['pfnBlocked'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnBlocked'] do
-    pcall(engine_callback.callbacks['pfnBlocked'][i], E, E2)
-  end
-end
-
-engine_events['pfnKeyValue'] = function(E,pkv)
-  if engine_callback.callbacks['pfnKeyValue'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnKeyValue'] do
-    pcall(engine_callback.callbacks['pfnKeyValue'][i], E, pkv)
-  end
-end
-
-engine_events['pfnClientConnect'] = function(E, name, addr)
-  if engine_callback.callbacks['pfnClientConnect'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientConnect'] do
-    pcall(engine_callback.callbacks['pfnClientConnect'][i], E, name, addr)
-  end
-end
-
-engine_events['pfnClientDisconnect'] = function(E)
-  if engine_callback.callbacks['pfnClientDisconnect'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientDisconnect'] do
-    pcall(engine_callback.callbacks['pfnClientDisconnect'][i], E)
-  end
-end
-
-engine_events['pfnClientKill'] = function(E)
-  if engine_callback.callbacks['pfnClientKill'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientKill'] do
-    pcall(engine_callback.callbacks['pfnClientKill'][i], E)
-  end
-end
-
-engine_events['pfnClientPutInServer'] = function(E)
-  if engine_callback.callbacks['pfnClientPutInServer'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientPutInServer'] do
-    pcall(engine_callback.callbacks['pfnClientPutInServer'][i], E)
-  end
-end
-
-engine_events['pfnClientUserInfoChanged'] = function (E, buffer)
-  if engine_callback.callbacks['pfnClientUserInfoChanged'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientUserInfoChanged'] do
-    pcall(engine_callback.callbacks['pfnClientUserInfoChanged'][i], E, buffer)
-  end
-end
-
-engine_events['pfnClientCommand'] = function(E, args, text)
-  if engine_callback.callbacks['pfnClientCommand'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnClientCommand'] do
-    pcall(engine_callback.callbacks['pfnClientCommand'][i], E, args, text)
-  end
-end
 --[[
-engine_events['pfnPlayerPreThink'] = function(E)
-  -- body
-end
-
-engine_events['pfnPlayerPostThink'] = function(E)
-  -- body
-end
+engine_events['pfnPlayerPreThink'] = function(E) end
+engine_events['pfnPlayerPostThink'] = function(E) end
 ]]--
-engine_events['pfnStartFrame'] = function()
-  if engine_callback.callbacks['pfnStartFrame'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnStartFrame'] do
-    pcall(engine_callback.callbacks['pfnStartFrame'][i])
-  end
-end
 
-engine_events['pfnServerCommand'] = function (command)
-  -- body
-end
-
-engine_events['pfnCvarValue2'] = function(E, req, name, value)
-  if engine_callback.callbacks['pfnCvarValue2'] == nil then return end
-  for i = 1, #engine_callback.callbacks['pfnCvarValue2'] do
-    pcall(engine_callback.callbacks['pfnCvarValue2'][i], E, req, name, value)
-  end
-end
+engine_events['pfnStartFrame'] = function(...) do_action('pfnStartFrame', ...) end
+engine_events['pfnServerCommand'] = function (...) do_action('pfnServerCommand', ...) end
+engine_events['pfnCvarValue2'] = function(...) do_action('pfnCvarValue2', ...) end
