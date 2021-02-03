832fd34062ab9a03836d739c1d463c524dcafd47
AruMoon <anton17082003@gmail.com>
2021-02-03 10:11:24 +0000

    Plugins corrections

--- plugins/core/plugins/ad/main.lua
+++ plugins/core/plugins/ad/main.lua
@@ -4,8 +4,8 @@ local gmsgHudText = engine.reg_user_msg("HudText", -1);
 
 local function advert_task()
 	engine.message_begin(0, gmsgHudText, nil, nil)
-	engine.write_string(string.format("This server is using LuaMod version %s", LUAMOD_VERSION))
+	engine.write_string("Support server's owner: patreon.com/arumoon")
 	engine.message_end()
 end
 
-task.add(30, advert_task, task.infinite)
+task.add(60, advert_task, task.infinite)
--- plugins/core/plugins/invis/main.lua
+++ plugins/core/plugins/invis/main.lua
@@ -3,15 +3,17 @@ plugin.register("Mr0maks", "invis", "0.0.1", "invisible admin")
 local privilege = require("privilege")
 local edict = require("edict")
 
-cmd.register("client", "invis", function (E, argv, args)
+cmd.register("client", "lm_invis", function (E, argv, args)
 	if privilege.get(E) == "admin" then
 		local e = edict.to(E)
 		if e.v.rendermode == 5 then
 			engine.client_printf(E, 1, '[invis] invis disabled\n');
 			e.v.rendermode = 0
+			e.v.solid = 3
 		else
 			engine.client_printf(E, 1, '[invis] invis enabled\n');
 			e.v.rendermode = 5
+			e.v.solid = 0
 		end
 	end
 end)
